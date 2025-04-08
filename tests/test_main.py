e939ed4752fd09687d419fcefafaf4c80c649763
Daniil Lamonov <lamonovdaniil@icloud.com>
2025-04-08 13:03:50 +0300

    Fix bugs

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,20 @@
 from datetime import datetime
-
-from fastapi.testclient import TestClient
-from app.main import app
+from tests.conftest import client
 from app.db.models.wallet import Wallet
 
 
 def test_wallet_insert():
     from app.db.session import get_db
     db = next(get_db())
-    record = Wallet(address="TXYZ123", bandwidth=100, energy=200, balance=300.5, date_added=datetime.now())
-    db.add(record)
+    query = Wallet(address="Tfdg443", bandwidth=100, energy=200, balance=300.5, date_added=datetime.now())
+    db.add(query)
     db.commit()
     found = db.query(Wallet).filter(Wallet.address == "TXYZ123").first()
     assert found is not None
     assert found.balance == 300.5
 
-def test_get_wallets_endpoint():
-    client = TestClient(app)
-    response = client.get("/wallets")
+def test_get_wallets_endpoint(client):
+    response = client.get("/wallets", params={"limit": 1})
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+    assert len(response.json()) == 1
